104fbbe53f29f0499c9ebb2d109cddc4ca0d7676
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-07-13 16:12:28 +0900

    사이드바 추가

--- front/src/components/ui/SideMenu.tsx
+++ front/src/components/ui/SideMenu.tsx
@@ -1,8 +1,32 @@
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SideMenu({ sideMenu, setSideMenu }) {
-    return (<div className={`flex w-full z-20 h-screen fixed transition-all duration-700 right-0 top-0 ${sideMenu?'max-w-full':'max-w-0'} overflow-hidden`} >
-        <div onClick={() => { setSideMenu(false) }} className="flex-1 h-full bg-[rgba(0,0,0,0.6)]"></div>
-        <div className="w-80 h-full transition-all duration-300 bg-white"></div>
-    </div>)
+  const [isVisible, setIsVisible] = useState(sideMenu);
+
+  useEffect(() => {
+    if (sideMenu) {
+      setIsVisible(true);
+    } else {
+      const timer = setTimeout(() => setIsVisible(false), 300);
+      return () => clearTimeout(timer);
+    }
+  }, [sideMenu]);
+
+  return (
+    <div className={`fixed inset-0 z-20 transition-all duration-300 ease-in-out ${isVisible ? 'visible' : 'invisible'}`}>
+      <div 
+        onClick={() => setSideMenu(false)} 
+        className={`absolute inset-0 bg-black transition-opacity duration-300 ease-in-out ${sideMenu ? 'bg-opacity-60' : 'bg-opacity-0'}`}
+      />
+      <div 
+        className={`absolute flex font-pretendard text-2xl font-semibold p-4 flex-col top-0 right-0 w-80 h-full bg-white transform transition-transform duration-300 ease-in-out ${sideMenu ? 'translate-x-0' : 'translate-x-full'}`}
+          >
+              <Link to={'/about'} className='mt-10 mb-4'>소개</Link>
+              <Link to={'/rank'} className='mb-4'>랭크</Link>
+              <Link to={'/notice'} className=''>공지사항</Link>
+
+      </div>
+    </div>
+  )
 }
