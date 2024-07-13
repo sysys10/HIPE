4a7002b3fa885e0ead77a701bb78600f6b13ccf0
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-07-13 15:35:51 +0900

    db comments

--- back/crawl/db.js
+++ back/crawl/db.js
@@ -1,11 +1,13 @@
 const Data_File = require("./Data_file");
 const { findTier } = require("./util");
 
-const add_to_user =  (data) => {
+const add_to_user = (data) => {
+  //유저에 넣는거..
   console.log(data);
 };
 
-const Add_to_solved =  ({ boj_id, solved }) => {
+const Add_to_solved = ({ boj_id, solved }) => {
+  //푼거에 넣는거..
   if (!Array.isArray(solved) || solved.length === 0) {
     console.error("No solved problems to add.");
     return;
@@ -20,6 +22,7 @@ const Add_to_solved =  ({ boj_id, solved }) => {
 };
 
 const Add_not_solved = (boj_id) => {
+  //안푼거에 넣는거...
   console.log(`${boj_id}님은 하나도 안풀었습니당`);
 };
 
